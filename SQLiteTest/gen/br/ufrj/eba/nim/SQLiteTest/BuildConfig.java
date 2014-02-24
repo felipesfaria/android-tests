2e9d18e5448892688aa4112286e5c1a800a7f4ed
felipesfaria <felipesfaria@hotmail.com>
2014-02-24 14:46:20 -0300

    Adicionei SQLiteTest
    SQLite teste é um prototipo pro menu da galeria.

---
+++ SQLiteTest/gen/br/ufrj/eba/nim/SQLiteTest/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package br.ufrj.eba.nim.SQLiteTest;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ SQLiteTest/src/br/ufrj/eba/nim/SQLiteTest/GaleriaMenu.java
@@ -0,0 +1,121 @@
+package br.ufrj.eba.nim.SQLiteTest;
+
+import java.io.IOException;  
+import java.util.ArrayList;
+import java.util.List;
+
+import android.app.Activity;  
+import android.database.Cursor;  
+import android.os.Bundle;  
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;  
+import android.widget.SimpleCursorAdapter;  
+
+public class GaleriaMenu extends Activity {  
+	/** Called when the activity is first created. */  
+	DBHelper dbhelper;  
+	Boolean[] ativos = new Boolean[] {true, false, false, false};
+	final int ARTISTAS = 0, TIPOS=1, OBRAS=2, FAVORITOS=3;
+	int estado;
+	@Override  
+	public void onCreate(Bundle savedInstanceState) {  
+		super.onCreate(savedInstanceState);  
+		setContentView(R.layout.main);
+		// if you use simplecursoradapter then you should have _id as one of column name and its values should be integer in your db.  
+		// so "_id", "columnName1", "columnName2" are column names from your db.
+		estado=ARTISTAS;
+		listarArtistas();
+	}  
+
+
+	void listarArtistas(){  
+		String[] from = new String[] {"nome"};  
+		int[] to = new int[] { R.id.TextView1};
+		List<String> arr_Artistas;
+
+		dbhelper = new DBHelper(this);  
+		try {  
+			dbhelper.createDataBase();  
+		} catch (IOException e) {  
+			// TODO Auto-generated catch block  
+			e.printStackTrace();  
+		}  
+
+		Cursor c = dbhelper.getArtistas();
+		c.moveToFirst();
+		arr_Artistas = new ArrayList<String>();
+		arr_Artistas.add("Todos");
+		int i=1;
+		for(c.moveToFirst(); !c.isAfterLast(); c.moveToNext()) {
+			// The Cursor is now set to the right position
+			arr_Artistas.add(c.getString(1));
+		}
+
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,R.layout.list,arr_Artistas);
+		//	  SimpleCursorAdapter adapter = new SimpleCursorAdapter(  
+		//	    getApplicationContext(), R.layout.list, c, from, to);  
+
+		ListView list = (ListView) findViewById(R.id.ListView1);  
+		list.setAdapter(adapter);
+	}
+
+	void listarTipos(){  
+		String[] from = new String[] {"nome"};  
+		int[] to = new int[] { R.id.TextView1};  
+
+		dbhelper = new DBHelper(this);  
+		try {  
+			dbhelper.createDataBase();  
+		} catch (IOException e) {  
+			// TODO Auto-generated catch block  
+			e.printStackTrace();  
+		}  
+
+		Cursor c = dbhelper.getTipos("todos");
+
+		SimpleCursorAdapter adapter = new SimpleCursorAdapter(  
+				getApplicationContext(), R.layout.list, c, from, to);  
+
+		ListView list = (ListView) findViewById(R.id.ListView1);  
+
+		list.setAdapter(adapter);
+	}
+
+	public void btn_artistas_onClick(View v){
+		Log.d("nim","Botao artistas clickado");
+
+		if(estado!=ARTISTAS){
+			estado=ARTISTAS;
+			listarArtistas();
+		}
+	}
+
+	public void btn_tecnicas_onClick(View v){
+		Log.d("nim","Botao tecnicas clickado");
+
+		if(estado!=TIPOS){
+			estado=TIPOS;
+			listarTipos();
+		}
+	}
+
+	public void btn_obras_onClick(View v){
+		Log.d("nim","Botao obras clickado");
+
+		if(estado!=OBRAS){
+			estado=OBRAS;
+			//listarObras();
+		}
+	}
+
+	public void btn_favoritos_onClick(View v){
+		Log.d("nim","Botao favoritos clickado");
+
+		if(estado!=FAVORITOS){
+			estado=FAVORITOS;
+			//listarFavoritos();
+		}
+	}
+}  
